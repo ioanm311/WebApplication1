022de4d3e10e9787a5f351a755da8e88b77e4151
Nastase Marius <mnastase19@yahoo.com>
2024-01-15 14:34:56 +0200

    added _Layout changes + cart functionalities + 'Despre' page

--- WebApplication1/Pages/Cart/Index.cshtml.cs
+++ WebApplication1/Pages/Cart/Index.cshtml.cs
@@ -1,12 +1,105 @@
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
+using WebApplication1.Data;
+using static ShopModel;
 
 namespace WebApplication1.Pages.Cart
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<IndexModel> _logger; // Logger
+        public decimal TotalPrice { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public string ErrorMessage { get; set; }
+
+
+        public IndexModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId.HasValue)
+            {
+                ShoppingCartItems = _context.ShoppingCart
+                            .Where(cart => cart.UserId == userId.Value)
+                            .Select(cartItem => new ShoppingCartItem
+                            {
+                                ShoppingCartId = cartItem.ShoppingCartId,
+                                ProductName = cartItem.ProductName,
+                                Quantity = cartItem.Quantity,
+                                Price = cartItem.Price
+                            }).ToList();
+                TotalPrice = ShoppingCartItems.Sum(item => item.Price * item.Quantity);
+                return Page();
+            }
+            else
+            {
+                // Setează TotalPrice la 0 dacă nu există un userId valid sau dacă nu sunt articole în coș
+                TotalPrice = 0;
+                return RedirectToPage("/Auth/Login");
+            }
+        }
+
+        public async Task<IActionResult> OnPostStergereItemAsync(int itemId)
+        {
+           
+
+            var item = await _context.ShoppingCart.FindAsync(itemId);
+            if (item != null)
+            {
+                _context.ShoppingCart.Remove(item);
+                await _context.SaveChangesAsync();
+                
+            }
+
+            await OnGetAsync();
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostApplyDiscountAsync(string discountCode)
+        {
+            if (string.IsNullOrWhiteSpace(discountCode))
+            {
+                // Dacă discountCode este null, gol sau spații albe, doar reîncarcă pagina.
+                await OnGetAsync();
+                return Page();
+            }
+
+            await OnGetAsync(); // Reîncarcă elementele din coș
+
+            if (ShoppingCartItems.Any())
+            {
+                switch (discountCode.ToUpper())
+                {
+                    case "B5":
+                        TotalPrice *= 0.95m; // Reducere de 5%
+                        break;
+                    case "B10":
+                        TotalPrice *= 0.90m; // Reducere de 10%
+                        break;
+                    case "B15":
+                        TotalPrice *= 0.85m; // Reducere de 15%
+                        break;
+                    case "B25":
+                        TotalPrice *= 0.80m; // Reducere de 15%
+                        break;
+                    case "B35":
+                        TotalPrice *= 0.75m; // Reducere de 15%
+                        break;
+                    default:
+                        ErrorMessage = "Cod invalid";
+                        break;
+                }
+            }
+
+            return Page();
         }
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebf504e259d6d679759fa5f5c9f0eb5ffc26b37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1de00eb2f418c58d554310b522aa070eaaf0fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
